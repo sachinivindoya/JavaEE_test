afcfa0f6afe78a98a7051b496b966e170b927cd1
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-07-28 16:29:34 +0530

    validation

--- src/main/java/lk/ijse/javaee_test/validation/StudentValidation.java
+++ src/main/java/lk/ijse/javaee_test/validation/StudentValidation.java
@@ -1,4 +1,26 @@
 package lk.ijse.javaee_test.validation;
 
+import lk.ijse.javaee_test.dto.StudentDTO;
+
 public class StudentValidation {
+
+    public static boolean studentValidation(StudentDTO studentDTO){
+        if (studentDTO.getName() == null || studentDTO.getName().matches("[A-Za-z]+")) {
+            if (studentDTO.getCity() == null || studentDTO.getCity().matches("[A-Za-z]+")) {
+                if (studentDTO.getEmail() == null || studentDTO.getEmail().matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")) {
+                    if (studentDTO.getLevel() >= 0) {
+                        return true;
+                    }else{
+                        throw new RuntimeException("Invalid Level");
+                    }
+                }else {
+                    throw new RuntimeException("Invalid Email");
+                }
+            }else {
+                throw new RuntimeException("Invalid City");
+            }
+        }else {
+            throw new RuntimeException("Invalid Name");
+        }
+    }
 }
